165934f8f22959d64ccfcad2215ef91bf1c34e83
Madison Carr <madison.carr@utexas.edu>
2020-12-03 10:14:18 -0600

    Renames user and authority tables

--- src/main/java/com/games/online/shop/domain/User.java
+++ src/main/java/com/games/online/shop/domain/User.java
@@ -20,7 +20,7 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
  * A user.
  */
 @Entity
-@Table(name = "jhi_user")
+@Table(name = "shop_user")
 @org.springframework.data.elasticsearch.annotations.Document(indexName = "user")
 public class User extends AbstractAuditingEntity implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -83,7 +83,7 @@ public class User extends AbstractAuditingEntity implements Serializable {
     @JsonIgnore
     @ManyToMany
     @JoinTable(
-        name = "jhi_user_authority",
+        name = "user_authority",
         joinColumns = { @JoinColumn(name = "user_id", referencedColumnName = "id") },
         inverseJoinColumns = { @JoinColumn(name = "authority_name", referencedColumnName = "name") }
     )
